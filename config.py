db8147a529fe539d31d2cc0535915d8cc2148e1e
GENZ-ARSH <arshtyagi007@gmail.com>
2025-08-10 23:11:48 +0530

    Prepare for Heroku deploy with buildpacks

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/dev-trezlo/OLYN",
+    "https://github.com/dev-trezlo/lucimusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
